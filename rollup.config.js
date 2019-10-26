baeffdce4f641f91260a2abe3f4afef10f1e62b8
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2019-10-26 03:59:54 +0330

    minor

--- rollup.config.js
+++ rollup.config.js
@@ -11,8 +11,8 @@ const modulepreloadPlugin = {
 		const paths = Object.keys(bundle['main.tmp.js'].modules).map( i => relative('./public', i).replace(/\\/g, '/') );
 		const modulepreloads = paths.map(i => `<link rel="modulepreload" href="{{root}}${i}" />`).join('\n');
 		const app = paths.map(i => `<script type="module" src="{{root}}${i}"></script>`).join('\n');
-		writeFileSync('html/link-modulepreload/main.handlebars', modulepreloads);
-		writeFileSync('html/script-app/main.handlebars', temps + app);
+		writeFileSync('./html/link-modulepreload/main.handlebars', modulepreloads);
+		writeFileSync('./html/script-app/main.handlebars', temps + app);
 		delete bundle['main.tmp.js']; // prevent creation of bundle
 	}
 };
