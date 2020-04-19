dae5ee92225c816316b24551ed91249e3aaf8a22
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2020-04-20 04:14:07 +0430

    updated watch.cmd
    added `left` and `step` variables for readability
    added laptop sizes in comment

--- watch.cmd
+++ watch.cmd
@@ -2,7 +2,7 @@
 start wp.cmd
 start wt.cmd
 start wl.cmd
-REM start wsr.cmd (L16=150 L18=1420 L21=8)
+REM start wsr.cmd (top=150 count==8)
 timeout 1
 start ws.cmd
 start wj.cmd
@@ -14,11 +14,14 @@ if %errorlevel% == 1 (echo cmdow not installed. && exit /b 1)
 setlocal EnableDelayedExpansion
 set count=0
 set top=267
+set left=1430
+set step=110
 for /f %%i in ('cmdow /t') do (
-	cmdow %%i /mov 1430 !top!
-	set /a "top=!top!+110"
+	cmdow %%i /mov !left! !top!
+	set /a "top=!top!+!step!"
 	set /a "count=!count!+1"
 	if "!count!" == "7" goto :done
 )
 :done
 setlocal DisableDelayedExpansion
+REM laptop: top=-35 left=875 step=95 (set.cmd: lines=4)
